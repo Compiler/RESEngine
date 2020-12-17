fb962890505ff53c9834fbf3ec99ad58c42716a5
compiler <lukerochedev@gmail.com>
2020-12-17 10:25:51 -0500

    rendering w/ blinn

--- src/Res/Tools/SceneState/DebugLayer.cpp
+++ src/Res/Tools/SceneState/DebugLayer.cpp
@@ -25,7 +25,6 @@ namespace res{
         glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (const void*)0);
         glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (const void*)(4 * sizeof(float)));
 
-        trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
         trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5)); 
 
         
